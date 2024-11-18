edc2f9abed57b3733270645a9676f2eca1af6835
madlou <madlou@gmail.com>
2024-11-18 10:15:28 +0000

    removed stub code

--- src/main/java/com/tjx/lew00305/slimstore/service/TransactionService.java
+++ src/main/java/com/tjx/lew00305/slimstore/service/TransactionService.java
@@ -4,21 +4,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
-    
-    public String[] getCurrentAuditLog() {
-        return new String[] {
-                "980/0971 Energizer Battery",
-                "1@ £2.79",
-                "",
-                "980/3064 Duracell Battery",
-                "1@ £3.79",
-                "",
-                "365/0376 Play Sand",
-                "1@ £4.99",
-                "",
-                "Items: 3",
-                "Total: £11.57"
-        };
-    }
 
 }
