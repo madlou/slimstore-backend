eaa08fba8835098f7a14c3effd87f728d636e4ed
Lewis Matthews <madlou@gmail.com>
2024-12-11 23:52:14 +0000

    improved sql format

--- src/main/java/com/tjx/lew00305/slimstore/repository/TransactionRepository.java
+++ src/main/java/com/tjx/lew00305/slimstore/repository/TransactionRepository.java
@@ -13,34 +13,36 @@ import com.tjx.lew00305.slimstore.model.report.TransactionTenderAggregationInter
 
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
 
-    @Query(value = "SELECT " +
-        "s.number AS 'store', " +
-        "s.name AS 'storeName', " +
-        "DATE(x.date) AS 'date', " +
-        "r.number AS 'reg', " +
-        "t.type AS 'type', " +
-        "ROUND(SUM(t.value), 2) AS 'value' " +
-        "FROM store s " +
-        "LEFT JOIN store_register r ON (s.number = r.store_id) " +
-        "LEFT JOIN transaction x ON (r.id = x.register_id) " +
-        "LEFT JOIN transaction_tender t ON (x.id = t.transaction_id) " +
-        "WHERE " +
-        "r.number LIKE ?1 AND " +
-        "s.number LIKE ?2 AND " +
-        "DATE(x.date) >= ?3 AND " +
-        "DATE(x.date) <= ?4 " +
-        "GROUP BY " +
-        "s.number, " +
-        "s.name, " +
-        "DATE(x.date), " +
-        "r.number, " +
-        "t.type " +
-        "HAVING t.type != \"\" " + // (value < 0) OR (value > 0)
-        "ORDER BY " +
-        "s.number ASC, " +
-        "r.number ASC, " +
-        "DATE(x.date) ASC, " +
-        "t.type ASC ", nativeQuery = true)
+    @Query(value = """
+            SELECT
+            s.number AS 'store',
+            s.name AS 'storeName',
+            DATE(x.date) AS 'date',
+            r.number AS 'reg',
+            t.type AS 'type',
+            ROUND(SUM(t.value), 2) AS 'value'
+            FROM store s
+            LEFT JOIN store_register r ON (s.number = r.store_id)
+            LEFT JOIN transaction x ON (r.id = x.register_id)
+            LEFT JOIN transaction_tender t ON (x.id = t.transaction_id)
+            WHERE
+            r.number LIKE ?1 AND
+            s.number LIKE ?2 AND
+            DATE(x.date) >= ?3 AND
+            DATE(x.date) <= ?4
+            GROUP BY
+            s.number,
+            s.name,
+            DATE(x.date),
+            r.number,
+            t.type
+            HAVING t.type != ""
+            ORDER BY
+            s.number ASC,
+            r.number ASC,
+            DATE(x.date) ASC,
+            t.type ASC
+        """, nativeQuery = true)
     List<TransactionTenderAggregationInterface> aggregateTenders(
         String reg,
         String store,
